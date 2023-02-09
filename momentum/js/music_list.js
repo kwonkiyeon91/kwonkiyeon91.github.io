581a2ff8ddcbb72e383a36f75ca44ae0032274a3
kiyeon <kky925@gmail.com>
2023-02-09 15:26:12 +0900

    230209 추가중...

---
+++ momentum/js/music_list.js
@@ -0,0 +1,26 @@
+let music =[
+    {
+        title:"도서관",
+        path: "./music/도서관.mp3",
+        artist: "artist",
+        cover: "./music/library.jpg"
+    },
+    {
+        title:"모닥불",
+        path: "./music/모닥불.mp3",
+        artist: "artist",
+        cover: "./music/campfire.jpg"
+    },
+    {
+        title:"빗소리",
+        path: "./music/빗소리.mp3",
+        artist: "artist",
+        cover: "./music/rain.jpg"
+    },
+    {
+        title:"자연",
+        path: "./music/자연.mp3",
+        artist: "artist",
+        cover: "./music/nature.jpg"
+    }
+]
--- momentum/js/quotes.js
+++ momentum/js/quotes.js
@@ -1,54 +1,65 @@
 const quotes = [
-    {
-        quote: "All progress takes place outside the comfort zone.",
-        author: "Michael John Bobak"
-    },
-    {
-        quote: "Success usually comes to those who are too busy to be looking for it",
-        author: "Henry David Thoreau"
-    },
-    {
-        quote: "The way to get started is to quit talking and begin doing.",
-        author: "Walt Disney"
-    },
-    {
-        quote: "In order to succeed, we must first believe that we can.",
-        author: "Nikos Kazantzakis"
-    },
-    {
-        quote: "life is not fair get used to it.",
-        author: "Bill Gates"
-    },
-    {
-        quote: "Only I can change my life. No one can do it for me.",
-        author: "Carol Burnett"
-    },
-    {
-        quote: "To improve is to change; to be perfect is to change often.",
-        author: "Winston Churchill"
-    },
-    {
-        quote: "It's not that I'm so smart , it's just that I stay with problems longer.",
-        author: "Socrates"
-    },
-    {
-        quote: "Yesterday's home runs don't win today's games.",
-        author: "Babe Ruth"
-    },
-    {
-        quote: "Live as if you were to die tomorrow. Learn as if you were to live forever.",
-        author: "Mahatma Gandhi"
-    },
-    {
-        quote: "The future depends on what we do in the present.",
-        author: "Mahatma Gandhi"
-    }
-]
+  {
+    quote: "All progress takes place outside the comfort zone.",
+    author: "Michael John Bobak",
+  },
+  {
+    quote:
+      "Success usually comes to those who are too busy to be looking for it",
+    author: "Henry David Thoreau",
+  },
+  {
+    quote: "The way to get started is to quit talking and begin doing.",
+    author: "Walt Disney",
+  },
+  {
+    quote: "In order to succeed, we must first believe that we can.",
+    author: "Nikos Kazantzakis",
+  },
+  {
+    quote: "life is not fair get used to it.",
+    author: "Bill Gates",
+  },
+  {
+    quote: "Only I can change my life. No one can do it for me.",
+    author: "Carol Burnett",
+  },
+  {
+    quote: "To improve is to change; to be perfect is to change often.",
+    author: "Winston Churchill",
+  },
+  {
+    quote:
+      "It's not that I'm so smart , it's just that I stay with problems longer.",
+    author: "Socrates",
+  },
+  {
+    quote: "Yesterday's home runs don't win today's games.",
+    author: "Babe Ruth",
+  },
+  {
+    quote:
+      "Live as if you were to die tomorrow. Learn as if you were to live forever.",
+    author: "Mahatma Gandhi",
+  },
+  {
+    quote: "The future depends on what we do in the present.",
+    author: "Mahatma Gandhi",
+  },
+];
 
 const quote = document.querySelector("#quote span:first-child");
-const author = document.querySelector("#quote span:last-child");
+const author = document.querySelector("#quote span.author");
+const copys = document.querySelector("#quote span.copy");
 
 const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
 quote.innerText = todaysQuote.quote;
 author.innerText = todaysQuote.author;
+
+copys.addEventListener("click", ()=>{
+    const text = `${quote.innerText} - ${author.innerText}`;
+    navigator.clipboard.writeText(text).then(function(){
+        window.alert("복사 되었습니다!");
+    })
+})
